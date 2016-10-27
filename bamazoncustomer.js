56b581df7a0a0cc8bf1912ab31fa97790569f47f
mikedeleamon <ma3971a@student.american.edu>
2016-10-27 18:52:46 -0400

    initial commit

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -15,12 +15,12 @@ var connection = mysql.createConnection({
 // this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
 var runSearch = function() {
     inquirer.prompt({
-        name: "action",
-        type: "list",
+        name: "item",
+        type: "input",
         message: "What would you like to buy?",
-        choices: ["Find songs by artist", "Find all artists who appear more than once", "Find data within a specific range", "Search for a specific song"]
+        
     }).then(function(answer) {
-        var quantity = 'SELECT * FROM Produts ';
+        var quantity = 'SELECT * FROM Produts ProductName '+answer+';' ;
         }
     })
 };
@@ -36,10 +36,11 @@ var showALL = function() {
 };
 
 
+
 //The app should then prompt users with two messages.
 ////The first should ask them the ID of the product they would like to buy.
 ////The second message should ask how many units of the product they would like to buy
-
+showAll();
 
 
 //Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
